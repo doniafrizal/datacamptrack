af0fa528cc2ec1c0416bdf98b1493f5515cfe279
doniafrizal <doni.afrizal@gmail.com>
2020-05-31 22:25:26 +0700

    read_excel from multisheet

--- 03._Import_Clean_Data/05._Data_Ingestion_Pandas/read_excel.py
+++ 03._Import_Clean_Data/05._Data_Ingestion_Pandas/read_excel.py
@@ -25,6 +25,28 @@ print(survey_responses.columns)
 
 #%%
 
-# Getting data from multiple worksheets
+# Getting data from multiple worksheets, now read sheet 1 ('2017')
 
+# Create df from second worksheet by referencing its position
+responses_2017 = pd.read_excel(os.path.join('00._data', 'fcc-new-coder-survey.xlsx'),
+                               sheet_name=1,
+                               skiprows=2,
+                               usecols=col_string)
 
+# Graph where people would like to get a developer job
+job_prefs = responses_2017.groupby("JobPref").JobPref.count()
+job_prefs.plot.barh()
+plt.show()
+
+#%%
+
+# Create df from second worksheet by referencing its name
+responses_2017 = pd.read_excel(os.path.join('00._data', 'fcc-new-coder-survey.xlsx'),
+                               sheet_name='2017',
+                               skiprows=2,
+                               usecols=col_string)
+
+# Graph where people would like to get a developer job
+job_prefs = responses_2017.groupby("JobPref").JobPref.count()
+job_prefs.plot.barh()
+plt.show()
